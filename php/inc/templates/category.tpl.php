07d3351f5185f925d0e3a7c2847c7ddd518b9371
Olivier340 <46643169+Olivier340@users.noreply.github.com>
2023-07-01 11:52:48 +0200

    correction aside

--- php/inc/templates/category.tpl.php
+++ php/inc/templates/category.tpl.php
@@ -8,21 +8,21 @@
         https://getbootstrap.com/docs/4.1/layout/grid/#grid-options -->
       <main class="col-lg-9">
 
-      <h1>Page <?= $categorieToDisplay->title?> </h1>
-     </div>
-    
-     <?php $articlesList = $dataArticlesList; 
-            $articles = $categorieToDisplay->articles($articlesList);?>
+        <h1>Page <?= $categorieToDisplay->title ?> </h1>
 
-     <?php foreach ($articles as $key => $articleObject) :?>
-                <article class="card">
-                    <div class="card-body">
-                    <h2 class="card-title"><a href="index.php?page=article&id=<?= $key ?>"><?= $articleObject->title ?></a></h2>
-                    <p class="infos">
-                        Posté par <a href="#" class="card-link"><?= $articleObject->author ?></a> le <time datetime="2017-07-13"><?= $articleObject->date ?> </time><?php $articleObject->date ?></time> dans <a href="#"
-                        class="card-link"><?= $articleObject->category ?></a>
-                    </p>
-                    <p class="card-text"><?= $articleObject->content ?></p>
-                    </div>
-            </article>
-         <?php endforeach; ?> 
+
+        <?php $articlesList = $dataArticlesList;
+        $articles = $categorieToDisplay->articles($articlesList); ?>
+
+        <?php foreach ($articles as $key => $articleObject) : ?>
+          <article class="card">
+            <div class="card-body">
+              <h2 class="card-title"><a href="index.php?page=article&id=<?= $key ?>"><?= $articleObject->title ?></a></h2>
+              <p class="infos">
+                Posté par <a href="#" class="card-link"><?= $articleObject->author ?></a> le <time datetime="2017-07-13"><?= $articleObject->date ?> </time><?php $articleObject->date ?></time> dans <a href="#" class="card-link"><?= $articleObject->category ?></a>
+              </p>
+              <p class="card-text"><?= $articleObject->content ?></p>
+            </div>
+          </article>
+        <?php endforeach; ?>
+      </main>
--- php/inc/templates/home.tpl.php
+++ php/inc/templates/home.tpl.php
@@ -9,25 +9,24 @@
       <main class="col-lg-9">
 
 
-             <?php foreach ($articlesList as $articleId => $articleObject) : ?>
-         
-              <article class="card">
-              <div class="card-body">
-                <h2 class="card-title"><a href="index.php?page=article&id=<?= $articleId ?>"><?= $articleObject->title ?></a></h2>
-                <p class="infos">
-                  Posté par <a href="#" class="card-link"><?= $articleObject->author ?></a> le <time datetime="2017-07-13"><?= $articleObject->date ?> </time><?php $articleObject->date ?></time> dans <a href="#"
-                    class="card-link"><?= $articleObject->category ?></a>
-                </p>
-                <p class="card-text"><?= $articleObject->content ?></p>
-              </div>
-             </article>
+        <?php foreach ($articlesList as $articleId => $articleObject) : ?>
 
-            <?php endforeach; ?> 
+          <article class="card">
+            <div class="card-body">
+              <h2 class="card-title"><a href="index.php?page=article&id=<?= $articleId ?>"><?= $articleObject->title ?></a></h2>
+              <p class="infos">
+                Posté par <a href="#" class="card-link"><?= $articleObject->author ?></a> le <time datetime="2017-07-13"><?= $articleObject->date ?> </time><?php $articleObject->date ?></time> dans <a href="#" class="card-link"><?= $articleObject->category ?></a>
+              </p>
+              <p class="card-text"><?= $articleObject->content ?></p>
+            </div>
+          </article>
 
-               
+        <?php endforeach; ?>
 
 
-             <!-- Je met un element de navigation: https://getbootstrap.com/docs/4.1/components/pagination/ -->
+
+
+        <!-- Je met un element de navigation: https://getbootstrap.com/docs/4.1/components/pagination/ -->
         <nav aria-label="Page navigation example">
           <ul class="pagination justify-content-between">
             <li class="page-item"><a class="page-link" href="#"><i class="fa fa-arrow-left"></i> Précédent</a></li>
@@ -36,5 +35,3 @@
         </nav>
 
       </main>
-
-    
