492db011ca2272475a428e36141e9cdea50bda1d
Olivier340 <46643169+Olivier340@users.noreply.github.com>
2023-07-05 05:16:56 +0200

    add readme.html

--- php/inc/templates/aside.tpl.php
+++ php/inc/templates/aside.tpl.php
@@ -1,30 +1,29 @@
 <aside class="col-lg-3">
-        <!-- Champ de recherche: https://getbootstrap.com/docs/4.1/components/input-group/ -->
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Rechercher un article..."
-            aria-label="Rechercher un article..." aria-describedby="basic-addon2">
-          <div class="input-group-append">
-            <button class="btn btn-outline-secondary" type="button">Allez</button>
-          </div>
-        </div>
+  <!-- Champ de recherche: https://getbootstrap.com/docs/4.1/components/input-group/ -->
+  <div class="input-group mb-3">
+    <input type="text" class="form-control" placeholder="Rechercher un article..." aria-label="Rechercher un article..." aria-describedby="basic-addon2">
+    <div class="input-group-append">
+      <button class="btn btn-outline-secondary" type="button">Allez</button>
+    </div>
+  </div>
 
-        <!-- Catégories: https://getbootstrap.com/docs/4.1/components/card/#list-groups-->
-        <div class="card">
-          <h3 class="card-header">Catégories</h3>
-            <?php foreach ($categorieList as $categorieId => $categorieObject) : ?>
-             <a class="list-group-item list-group-item-action list-group-item" href="../php/index.php?page=category&id=<?=$categorieId?>"><?=$categorieObject->title?></a>
-            <?php endforeach; ?>   
-        </div>
+  <!-- Catégories: https://getbootstrap.com/docs/4.1/components/card/#list-groups-->
+  <div class="card">
+    <h3 class="card-header">Catégories</h3>
+    <?php foreach ($categorieList as $categorieId => $categorieObject) : ?>
+      <a class="list-group-item list-group-item-action list-group-item" href="../php/index.php?page=category&id=<?= $categorieId ?>"><?= $categorieObject->title ?></a>
+    <?php endforeach; ?>
+  </div>
 
-        <!-- Auteurs: https://getbootstrap.com/docs/4.1/components/card/#list-groups -->
-        <div class="card">
-          <h3 class="card-header">Auteurs</h3>
-          <ul class="list-group list-group-flush">
-            <?php foreach ($authorList as $authorId => $authorObject) : ?>
-                <a class="list-group-item list-group-item-action list-group-item" href="../php/index.php?page=author&id=<?=$authorId?>"><?=$authorObject->name?></a>
-            <?php endforeach; ?>   
-          </ul>
-        </div>
+  <!-- Auteurs: https://getbootstrap.com/docs/4.1/components/card/#list-groups -->
+  <div class="card">
+    <h3 class="card-header">Auteurs</h3>
+    <ul class="list-group list-group-flush">
+      <?php foreach ($authorList as $authorId => $authorObject) : ?>
+        <a class="list-group-item list-group-item-action list-group-item" href="../php/index.php?page=author&id=<?= $authorId ?>"><?= $authorObject->name ?></a>
+      <?php endforeach; ?>
+    </ul>
+  </div>
 
-      </aside>
-      </div><!-- /.row -->
+</aside>
+</div><!-- /.row -->
--- php/inc/templates/author.tpl.php
+++ php/inc/templates/author.tpl.php
@@ -9,22 +9,22 @@
       <main class="col-lg-9">
 
         <h1>Auteur : <?= $authorToDisplay->name ?> </h1>
-   
-
-    <?php $articlesList = $dataArticlesList;
-    $articles = $authorToDisplay->articles($articlesList); ?>
-
-    <?php foreach ($articles as $key => $articleObject) : ?>
-      <article class="card">
-        <div class="card-body">
-          <h2 class="card-title"><a href="index.php?page=article&id=<?= $key ?>"><?= $articleObject->title ?></a></h2>
-          <p class="infos">
-            Posté par <a href="#" class="card-link"><?= $articleObject->author ?></a> le <time datetime="2017-07-13"><?= $articleObject->date ?> </time><?php $articleObject->date ?></time> dans <a href="#" class="card-link"><?= $articleObject->category ?></a>
-          </p>
-          <p class="card-text"><?= $articleObject->content ?></p>
-        </div>
-      </article>
-    <?php endforeach; ?>
 
 
-    </main>
+        <?php $articlesList = $dataArticlesList;
+        $articles = $authorToDisplay->articles($articlesList); ?>
+
+        <?php foreach ($articles as $key => $articleObject) : ?>
+          <article class="card">
+            <div class="card-body">
+              <h2 class="card-title"><a href="index.php?page=article&id=<?= $key ?>"><?= $articleObject->title ?></a></h2>
+              <p class="infos">
+                Posté par <a href="#" class="card-link"><?= $articleObject->author ?></a> le <time datetime="2017-07-13"><?= $articleObject->date ?> </time><?php $articleObject->date ?></time> dans <a href="#" class="card-link"><?= $articleObject->category ?></a>
+              </p>
+              <p class="card-text"><?= $articleObject->content ?></p>
+            </div>
+          </article>
+        <?php endforeach; ?>
+
+
+      </main>
